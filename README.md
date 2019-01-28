6b9dbe53daa1bf8aff4c356b6ff15b5d32229cf7
Nicco Kunzmann <niccokunzmann@rambler.ru>
2019-01-28 21:18:54 +0100

    add dynamic bdges

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 
 - https://github.com/sh4nks/flask-caching
 
+## Related Work
+
+- https://github.com/transifex/transifex/issues/319
+- https://github.com/greatislander/tenpercent
+
--- app.py
+++ app.py
@@ -10,7 +10,8 @@ import urllib.parse
 # configuration
 CACHE_TIMEOUT = int(os.environ.get("APP_CACHE_TIMEOUT", 60))
 TRANSIFEX_PASSWORD = os.environ["TRANSIFEX_PASSWORD"] # from https://www.transifex.com/user/settings/api/
-TRANSIFEX_USERNAME = os.environ.get("TRANSIFEX_USERNAME", "niccokunzmann3") # from https://www.transifex.com/user/settings/api/
+TRANSIFEX_USERNAME = os.environ.get("TRANSIFEX_USERNAME", "niccokunzmann3") # the user for the TRANSIFEX_PASSWORD
+SHIELDS_API = os.environ.get("SHIELDS_API", "https://img.shields.io") # replace this, if you run shields locally
 
 # constants
 AUTH = ("api", TRANSIFEX_PASSWORD)
@@ -18,8 +19,19 @@ EXAMPLE_ENTRIES = {
     "resource_slug": "07-better-to-be-hopeful-txt",
     "project_slug": "12-characters-play",
     "organization_slug": "12-characters",
-    "language": "de"
+    "language_code": "de",
+    "color": "green",
+    "label": "label"
 }
+TEMPLATE_ENTRIES = {
+    "resource_slug": "RESOURCE",
+    "project_slug": "PROJECT",
+    "organization_slug": "ORGANIZATION",
+    "language_code": "LANGUAGE",
+    "color": "COLOR",
+    "label": "LABEL"
+}
+PARAM_MODIFICATION = "modification"
 
 # globals
 app = Flask(__name__, template_folder= "templates")
@@ -37,7 +49,7 @@ def change_name(name):
         return function
     return change_to
 
-def open_api(name, role, url, documentation):
+def open_api(name, role, url, documentation, modifications={}):
     """Open the call to the API for a user."""
     
     # get the path from a url, see https://stackoverflow.com/a/7894483/1320237
@@ -51,33 +63,77 @@ def open_api(name, role, url, documentation):
     def get(**kw):
         # request query string, see http://werkzeug.pocoo.org/docs/0.14/wrappers/#werkzeug.wrappers.BaseRequest.args
         request_url = url_template.format(**kw)
+        args = request.args.copy()
+        if PARAM_MODIFICATION in args:
+            modification_name = args.pop(PARAM_MODIFICATION)
+            modification = modifications[modification_name]
+        else:
+            def modification(x):
+                return x
         print("request.path", request.path)
-        params = ("?" + urllib.parse.urlencode(request.args) if request.args else "")
-        print("curl -i -L --user api:$TRANSIFEX_PASSWORD -X GET '{}{}'".format(request_url, params))
-        response = requests.get(request_url, auth=AUTH, params=request.args)
+        params = ("?" + urllib.parse.urlencode(args) if args else "")
+        full_request_url = request_url + params
+        print("curl -i -L --user api:$TRANSIFEX_PASSWORD -X GET '{}'".format(full_request_url))
+        response = requests.get(request_url, auth=AUTH, params=args)
         result = response.json()
+        result = modification(result)
         # make a response, see https://stackoverflow.com/a/40211663/1320237
         resp = make_response(json.dumps(result, indent=2), ) #here you could use make_response(render_template(...)) too
         # flask response headers, see http://werkzeug.pocoo.org/docs/0.14/datastructures/#werkzeug.datastructures.Headers
+        for header in response.headers:
+            if header.lower().startswith("x-transifex"):
+                resp.headers[header] = response.headers[header]
         resp.headers['Access-Control-Allow-Origin'] = '*'
-        resp.headers['X-Documentation'] = documentation
-        resp.headers['X-Url'] = request_url
-        resp.headers['X-Url-Template'] = url
+        resp.headers['X-App-Documentation'] = documentation
+        resp.headers['X-App-Url'] = full_request_url
+        resp.headers['X-App-Url-Template'] = url
         resp.mimetype = "application/json"
         return resp
     
     example_url = "http://{host}" + urllib.parse.urlparse(url_template.format(**EXAMPLE_ENTRIES)).path
-    
+    mods = []
+    for modification_name in sorted(modifications):
+        function = modifications[modification_name]
+        mod_query = "?" + PARAM_MODIFICATION + "=" + modification_name
+        mods.append({
+            "name": modification_name,
+            "description": function.__doc__,
+            "example": example_url + mod_query,
+            "path": path + mod_query
+        })
     open_apis.append({
         "url": url,
         "documentation": documentation,
         "path": path,
         "name": name,
         "example": example_url,
-        "role": role
+        "role": role,
+        "get": get,
+        "modifications": mods
     })
     
-    
+def modify_resources_summary(data):
+    """Summarize the resources into one joint statistic."""
+    result = {
+        "stats": {
+            "reviewed_1":{
+                "name": "reviewed"
+            },
+            "translated":{
+                "name":"translated"
+            },
+            "language_code": data[0]["stats"].get("language_code")
+        }
+    }
+    for attribute in ["stringcount", "wordcount", "percentage"]:
+        if attribute in data[0]:
+            result[attribute] = sum(entry[attribute] for entry in data)
+        for stat in ["reviewed_1", "translated"]:
+            value = sum(entry["stats"][stat][attribute] for entry in data)
+            if attribute == "percentage":
+                value /= len(data) * 1.0
+            result["stats"][stat][attribute] = value
+    return result
 
 open_api(
     "Projects", "Translator", 
@@ -86,10 +142,11 @@ open_api(
 open_api(
     "Resources", "Translator", 
     "https://api.transifex.com/organizations/<organization_slug>/projects/<project_slug>/resources/",
-    "https://docs.transifex.com/api/resources")
+    "https://docs.transifex.com/api/resources",
+    modifications={"summary": modify_resources_summary})
 open_api(
     "Translations", "Translator", 
-    "https://www.transifex.com/api/2/project/<project_slug>/resource/<resource_slug>/translation/<language>/strings/",
+    "https://www.transifex.com/api/2/project/<project_slug>/resource/<resource_slug>/translation/<language_code>/strings/",
     "https://docs.transifex.com/api/translations")
 open_api(
     "Reports", "Translator", 
@@ -100,16 +157,104 @@ open_api(
     "https://www.transifex.com/api/2/project/<project_slug>/resource/<resource_slug>/stats/",
     "https://docs.transifex.com/api/statistics")
 open_api(
+    "Language Statistics", "Translator", 
+    "https://www.transifex.com/api/2/project/<project_slug>/resource/<resource_slug>/stats/<language_code>",
+    "https://docs.transifex.com/api/statistics")
+open_api(
     "Languages", "Project Maintainer", 
     "https://www.transifex.com/api/2/project/<project_slug>/languages/",
     "https://docs.transifex.com/api/languages")
+open_api(
+    "Organizations", "Translator", 
+    "https://api.transifex.com/organizations/",
+    "https://docs.transifex.com/api/organizations")
 
+dynamic_badges = []
+def dynamic_badge(name, description, app_path, query):
+    """Create a badge."""
+    def template_url(host, args=TEMPLATE_ENTRIES):
+        badge_query = {
+            "url": "http://" + host + app_path.format(**args),
+            "label": args["label"],
+            "query": query.format(**args),
+            "colorB": args["color"]}
+        for attr in ["prefix", "suffix"]:
+            if attr in args: 
+                badge_query[attr] = args[attr]
+        url = SHIELDS_API
+        url += "/badge/dynamic/json.svg?"
+        url += urllib.parse.urlencode(badge_query)
+        return url
+    
+    def example_url(host):
+        url = template_url(host, EXAMPLE_ENTRIES)
+        return url
+        
+    def show_url(host):
+        url = template_url(host)
+        for variable in TEMPLATE_ENTRIES.values():
+            url = url.replace(variable, "<strong>" + variable + "</strong>")
+        return url
+    dynamic_badges.append({ 
+        "name": name,
+        "description": description,
+        "app_path": app_path,
+        "query": query,
+        "example_url": example_url,
+        "template_url": template_url,
+        "show_url": show_url      
+    })
 
+dynamic_badge(
+    "Resource: overall translation",
+    "How much of the resource is translated.",
+    "/organizations/{organization_slug}/projects/{project_slug}/resources/",
+    '$[?(@.slug=="{resource_slug}")].stats[?(@.name=="translated")].percentage')
+dynamic_badge(
+    "Resource: translation of a language",
+    "How much of the resource is translated in one language.",
+    "/organizations/{organization_slug}/projects/{project_slug}/resources/?language_code={language_code}",
+    '$[?(@.slug=="{resource_slug}")].stats[?(@.name=="translated")].percentage')
+dynamic_badge(
+    "Project: overall translation",
+    "How much of a project is translated.",
+    "/organizations/{organization_slug}/projects/{project_slug}/resources/?modification=summary",
+    '$.stats[?(@.name=="translated")].percentage')
+dynamic_badge(
+    "Project: translation of a language",
+    "How much of a project is translated in a certain language.",
+    "/organizations/{organization_slug}/projects/{project_slug}/resources/?modification=summary&language_code={language_code}",
+    '$.stats[?(@.name=="translated")].percentage')
 
+dynamic_badge(
+    "Resource: overall review progress",
+    "How much of the resource is reviewed.",
+    "/organizations/{organization_slug}/projects/{project_slug}/resources/",
+    '$[?(@.slug=="{resource_slug}")].stats[?(@.name=="reviewed")].percentage')
+dynamic_badge(
+    "Resource: review progress of a language",
+    "How much of the resource is reviewed in one language.",
+    "/organizations/{organization_slug}/projects/{project_slug}/resources/?language_code={language_code}",
+    '$[?(@.slug=="{resource_slug}")].stats[?(@.name=="reviewed")].percentage')
+dynamic_badge(
+    "Project: overall review progress",
+    "How much of a project is reviewed.",
+    "/organizations/{organization_slug}/projects/{project_slug}/resources/?modification=summary",
+    '$.stats[?(@.name=="reviewed")].percentage')
+dynamic_badge(
+    "Project: review progress of a language",
+    "How much of a project is reviewed in a certain language.",
+    "/organizations/{organization_slug}/projects/{project_slug}/resources/?modification=summary&language_code={language_code}",
+    '$.stats[?(@.name=="reviewed")].percentage')
 
 @app.route("/", methods=['GET']) 
 def index():
-    return render_template("index.html", open_apis=open_apis, host=request.host, username=TRANSIFEX_USERNAME)
+    return render_template(
+        "index.html",
+        open_apis=open_apis,
+        host=request.host,
+        username=TRANSIFEX_USERNAME,
+        badges=dynamic_badges)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
